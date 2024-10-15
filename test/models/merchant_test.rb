5c6a31dbf9f5d70a2b81025c331cfca48473f038
Arkadiusz-Wrzawinski-DNA <arkadiusz.wrzawinski@digitalnewagency.com>
2024-10-15 15:44:49 +0200

    fix tests

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -2,23 +2,23 @@ require "test_helper"
 
 class MerchantTest < ActiveSupport::TestCase
   test "should not save merchant without a name" do
-merchant = Merchant.new merchantId: "TESTID"
-    assert_not _merchant.save
+    merchant = Merchant.new merchantId: "TESTID"
+    assert_not merchant.save
   end
 
   test "should not save merchant without a merchantId" do
-merchant = Merchant.new name: "TESTNAME"
-    assert_not _merchant.save
+    merchant = Merchant.new name: "TESTNAME"
+    assert_not merchant.save
   end
 
   test "should save merchant with a merchantId and a name" do
-merchant = Merchant.new merchantId: "TESTID", name: "TESTNAME"
-    assert _merchant.save
+    merchant = Merchant.new merchantId: "TESTID", name: "TESTNAME"
+    assert merchant.save
   end
 
   test "should create and save merchant with a random UUID as merchantId" do
-merchant = Merchant.create_with_uuid name: "TESTNAME"
-    assert _merchant.persisted?
-    assert_equal 36, _merchant.merchantId.length
+    merchant = Merchant.create_with_uuid name: "TESTNAME"
+    assert merchant.persisted?
+    assert_equal 36, merchant.merchantId.length
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,28 +7,28 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "should not save user without a fullName" do
-user = User.new userId: "TESTID", merchant_id: @merchant.id, email: "a@a"
-    assert_not _user.save
+    user = User.new userId: "TESTID", merchant_id: @merchant.id, email: "a@a"
+    assert_not user.save
   end
 
   test "should not save user without a merchantId" do
-user = User.new fullName: "TESTNAME", merchant_id: @merchant.id, email: "a@a"
-    assert_not _user.save
+    user = User.new fullName: "TESTNAME", merchant_id: @merchant.id, email: "a@a"
+    assert_not user.save
   end
 
   test "should not save user without an email" do
-user = User.new userId: "TESTID", fullName: "TESTNAME", merchant_id: @merchant.id
-    assert_not _user.save
+    user = User.new userId: "TESTID", fullName: "TESTNAME", merchant_id: @merchant.id
+    assert_not user.save
   end
 
   test "should save user with a userId and a fullName and an email" do
-user = User.new userId: "TESTID", fullName: "TESTNAME", merchant_id: @merchant.id, email: "a@a"
-    assert _user.save
+    user = User.new userId: "TESTID", fullName: "TESTNAME", merchant_id: @merchant.id, email: "a@a"
+    assert user.save
   end
 
-  test "should create and save user with a random UUID as userId" do
-user = User.create_with_uuid fullName: "TESTNAME", merchantId: @merchant.merchantId, email: "a@a"
-    assert _user.persisted?
-    assert_equal 36, _user.userId.length
+  test "should create and save user with a random UUID as userId" do 
+    user = User.create_with_uuid_and_account fullName: "TESTNAME", merchantId: @merchant.merchantId, email: "a@a"
+    assert user.persisted?
+    assert_equal 36, user.userId.length
   end
 end
