38942fd8cbbab7cf69ad892bf88243010e19ac8a
Arkadiusz-Wrzawinski-DNA <arkadiusz.wrzawinski@digitalnewagency.com>
2024-10-21 13:51:58 +0200

    routing changes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :merchants, only: :create
-  resources :users, only: :create
-  resources :payments, only: :create
+  resources :merchants
+  resources :users
+  resources :payments
 end
